096b866f2871cd0e8b1ec11a15fd61ee5c88615d
GaneshAdimalupu <ganeshjohn253@gmail.com>
2024-10-30 16:59:00 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,174 @@
+const logotext = "Ganesh";
+const meta = {
+    title: "Adimalupu Ganesh",
+    description: "I’m Ganesh Adimalupu, a 3rd-year student passionate about coding and developing cool projects.",
+};
+
+const introdata = {
+    title: "I’m Ganesh Adimalupu",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I'm a 3rd-year student studying [Your Course/Major] at [Your University]. I'm passionate about technology, coding, and developing innovative solutions. My goal is to continuously learn and grow in the field of software development.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "HTML/CSS",
+        value: 85,
+    },
+    {
+        name: "Node.js",
+        value: 75,
+    },
+];
+
+const services = [{
+    title: "Web Development",
+    description: "I develop responsive and user-friendly websites using modern web technologies like HTML, CSS, and JavaScript frameworks.",
+},
+{
+    title: "Mobile App Development",
+    description: "I have experience in developing mobile applications for Android and iOS platforms using React Native and other frameworks.",
+},
+{
+    title: "UI/UX Design",
+    description: "I design intuitive and visually appealing user interfaces to enhance the user experience of web and mobile applications.",
+},
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Ganeshjohn253@gmail.com",
+    YOUR_FONE: "+91 6303205936",
+    description: "Feel free to reach out to me via email or phone. I'm open to collaboration and new opportunities.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_3o9dvkk",
+    YOUR_TEMPLATE_ID: "template_cwuaoq7",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/GaneshAdimalupu",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/ganesh-adimalupu-62b407239/",
+    twitter: "https://twitter.com/John56247240",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
