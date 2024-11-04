ef631a3c21d8dce08bbf54f6f0fff082a75638ae
GaneshAdimalupu <ganeshjohn253@gmail.com>
2024-11-04 22:26:46 +0530

    welcome script at the home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,67 @@
-import React from "react";
-import "./style.css";
-import { Helmet, HelmetProvider } from "react-helmet-async";
-import Typewriter from "typewriter-effect";
-import { introdata, meta } from "../../content_option";
-import { Link } from "react-router-dom";
-import profileImage from "../../assets/images/profile.svg";
-import backgroundImage from "../../assets/images/background.svg"; // Import the background SVG
+import React, { useEffect, useState } from 'react';
+import './style.css';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import Typewriter from 'typewriter-effect';
+import { introdata, meta } from '../../content_option';
+import { Link } from 'react-router-dom';
+import profileImage from '../../assets/images/profile.svg';
+import backgroundImage from '../../assets/images/background.svg';
 
 export const Home = () => {
+  const [hoverEffect, setHoverEffect] = useState('');
+
+  // Define possible hover effects
+  const effects = [
+    'scale-color',
+    'text-glow',
+    'letter-spacing',
+    'rotate-gradient',
+  ];
+  // Function to randomly select an effect on hover
+  const randomizeEffect = () => {
+    const randomEffect = effects[Math.floor(Math.random() * effects.length)];
+    setHoverEffect(randomEffect);
+  };
+  // Reset effect on mouse leave
+  const resetEffect = () => {
+    setHoverEffect('');
+  };
+
+  const getRandomPosition = () => (Math.random() * 2 - 1).toFixed(2);
+
+  // Split text into spans with random positioning
+  const text = 'WELCOME TO MY PORTFOLIO';
+  const letters = text.split('').map((letter, index) => (
+    <span
+      key={index}
+      className="letter"
+      style={{
+        '--index': index,
+        '--random-x': getRandomPosition(),
+        '--random-y': getRandomPosition(),
+      }}
+    >
+      {letter}
+    </span>
+  ));
+
+  // Split words for the phrase and apply word spacing
+  const words = 'WELCOME TO MY PORTFOLIO'.split(' ').map((word, wordIndex) =>
+    word.split('').map((letter, letterIndex) => (
+      <span
+        key={`${wordIndex}-${letterIndex}`}
+        className="letter"
+        style={{
+          '--index': letterIndex + wordIndex * 3, // Update delay based on both word and letter position
+          '--random-x': getRandomPosition(),
+          '--random-y': getRandomPosition(),
+        }}
+      >
+        {letter}
+      </span>
+    )),
+  );
+
   return (
     <HelmetProvider>
       <section id="home" className="home">
@@ -16,16 +70,28 @@ export const Home = () => {
           <title> {meta.title}</title>
           <meta name="description" content={meta.description} />
         </Helmet>
+
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+          <h1
+            className={`display-4 text-center mb-4 animate-welcome ${hoverEffect}`}
+            onMouseEnter={randomizeEffect}
+            onMouseLeave={resetEffect}
+          >
+            {letters}{' '}
+          </h1>
+        </div>
+
         <div className="intro_sec d-block d-lg-flex align-items-center ">
           <div
             className="h_bg-image order-1 order-lg-2 h-100 "
-            style={{  backgroundImage: ` url(${profileImage}),url(${backgroundImage})`,
-            backgroundSize: "contain", 
-            backgroundPosition: "center", 
-            backgroundRepeat: "no-repeat", 
-            width: '500px', 
-            height:'500px' 
-             } }
+            style={{
+              backgroundImage: ` url(${profileImage}),url(${backgroundImage})`,
+              backgroundSize: 'auto,contain,100%',
+              backgroundPosition: 'center,center',
+              backgroundRepeat: 'no-repeat,no-repeat',
+              width: '600px',
+              height: '600px',
+            }}
           ></div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
